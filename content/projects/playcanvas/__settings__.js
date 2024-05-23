e6777c9140ea66a3f8cadb493c97fd90e5b8f16e
Hyperagon <23475800+hyperagon@users.noreply.github.com>
2024-05-23 14:13:44 +0000

    Add files via upload

---
+++ content/projects/playcanvas/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2006082.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 180980234, 180980235, 180980236, 180980237 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/180980258/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/180980259/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/180980257/1/ammo.js', 'preload' : true},
+];
